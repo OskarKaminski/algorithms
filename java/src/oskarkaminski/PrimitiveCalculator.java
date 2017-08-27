9e5c33bdce1c24f082a03f5f8ab4ac9ad4b4ff51
Oskar Kaminski <priv.oskar@gmail.com>
2017-08-27 20:28:02 +0200

    Reimplemented with dynamic programming

--- java/src/oskarkaminski/PrimitiveCalculator.java
+++ java/src/oskarkaminski/PrimitiveCalculator.java
@@ -1,30 +1,41 @@
 package oskarkaminski;
-
 import java.io.*;
-import java.util.Arrays;
 
 public class PrimitiveCalculator {
+
+    public static int minNumOfOperations(int number, int[] numOfOperations){
+        if(number == 1){
+            return 0;
+        }
+        double minNum = Double.POSITIVE_INFINITY;
+        if(number % 3 == 0){
+            int currentMin = numOfOperations[number/3];
+            if (currentMin + 1 < minNum){
+                minNum = currentMin + 1;
+            }
+        }
+        if(number % 2 == 0){
+            int currentMin = numOfOperations[number/2];
+            if (currentMin + 1 < minNum){
+                minNum = currentMin + 1;
+            }
+        }
+        int currentMin = numOfOperations[number-1];
+        if (currentMin + 1 < minNum){
+            minNum = currentMin + 1;
+        }
+        return (int)minNum;
+    }
+
     public static void main(String[] args) throws java.lang.Exception {
         BufferedReader r = new BufferedReader(new InputStreamReader(System.in));
         int number = Integer.parseInt(r.readLine());
-        int[] numbers = new int[100];
-        int i = 0;
-        while(number != 1){
-            if(number % 3 == 0){
-                numbers[i] = number / 3;
-                number /= 3;
-            }
-            else if(number % 2 == 0){
-                numbers[i] = number / 2;
-                number /= 2;
-            }
-            else {
-                numbers[i] = number - 1;
-                number -= 1;
-            }
-            i++;
+        int[] numOfOperations = new int[number+1];
+        numOfOperations[0] = 0;
+        for(int i = 1; i <= number; i++){
+            numOfOperations[i] = minNumOfOperations(i, numOfOperations);
         }
-        System.out.println(i);
-
+        int min = numOfOperations[number];
+        System.out.println(min);
     }
 }
